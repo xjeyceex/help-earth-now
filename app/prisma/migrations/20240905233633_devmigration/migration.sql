01c5c75c0f6cc8d0345bfd3ab015182cff9f32f6
Jc Miguel <jcmiguel.beltran@gmail.com>
2024-09-10 11:04:41 +0800

    correct some changes

--- app/prisma/migrations/20240905233633_devmigration/migration.sql
+++ app/prisma/migrations/20240905233633_devmigration/migration.sql
@@ -1,8 +1,8 @@
 -- CreateTable
 CREATE TABLE "users" (
     "id" TEXT NOT NULL PRIMARY KEY,
-    "firstName" TEXT NOT NULL,
-    "lastName" TEXT NOT NULL,
+    "firstName" TEXT,
+    "lastName" TEXT,
     "email" TEXT NOT NULL,
     "createDate" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updateDate" DATETIME NOT NULL

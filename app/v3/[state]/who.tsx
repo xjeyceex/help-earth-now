c64fed308e4b7e49b4d241fd15e98042a68af011
Jc Miguel <jcmiguel.beltran@gmail.com>
2024-10-05 16:11:12 +0800

    include president to all states

--- app/v3/[state]/who.tsx
+++ app/v3/[state]/who.tsx
@@ -3,16 +3,20 @@
 import Footer from "@/app/components/Footer";
 import Link from "next/link";
 import React from "react";
-import { useParams } from "next/navigation"; // Import useParams hook
+import { useParams } from "next/navigation"; 
 
-// Define types for candidates and groups
+enum Party {
+  Democratic = 'democratic',
+  Republican = 'republican',
+}
+
 interface Candidate {
   name: string;
   rating: string;
   description: string;
   link: string;
-  party: 'democratic' | 'republican'; // Consider using enum for better type safety
-  state: string;
+  party: Party;
+  state: string[]; 
 }
 
 interface CandidateGroup {
@@ -30,16 +34,16 @@ const candidates: CandidateGroup[] = [
         rating: '90% from LCV.',
         description: 'Calls climate crisis an urgent issue and promoted the IRA - spending $20B to fight climate change',
         link: 'https://www.whitehouse.gov/briefing-room/speeches-remarks/2023/07/14/remarks-by-vice-president-harris-on-combatting-climate-change-and-building-a-clean-energy-economy/',
-        party: 'democratic',
-        state: 'California', 
+        party: Party.Democratic,
+        state: ['Florida', 'All']
       },
       {
         name: 'Donald Trump',
         rating: '30% from LCV.',
         description: 'Pulled U.S. out of the Paris Agreement and rolled back efforts to fight climate change',
         link: 'https://www.npr.org/2024/06/25/nx-s1-5006573/trump-election-2024-climate-change-fossil-fuels',
-        party: 'republican',
-        state: 'Florida', 
+        party: Party.Republican,
+        state: ['Florida', 'All']
       },
     ],
   },
@@ -51,64 +55,64 @@ const candidates: CandidateGroup[] = [
         rating: '85% from LCV.',
         description: 'Strong proponent of addressing climate change; emphasizes its impact on communities.',
         link: 'https://www.npr.org/2024/06/25/nx-s1-5006573/trump-election-2024-climate-change-fossil-fuels',
-        party: 'democratic',
-        state: 'Florida', 
+        party: Party.Democratic,
+        state: ['Florida'],
       },
       {
         name: 'Rick Scott',
         rating: '40% from LCV.',
         description: 'Generally skeptical of climate change; prioritizes economic interests.',
-        party: 'republican',
+        party: Party.Republican,
         link: 'https://www.politifact.com/article/2015/mar/11/fact-checking-rick-scott-environment-and-sea-level/',
-        state: 'Florida', 
+        state: ['Florida'],
       },
       {
         name: 'Joe Manchin',
         rating: 'Moderate ratings from environmental groups',
         description: 'Advocates for energy independence, recognizes climate issues.',
-        party: 'democratic',
+        party: Party.Democratic,
         link: 'https://www.vox.com/climate/23955967/joe-manchin-climate-change-senate-biden-inflation',
-        state: 'West Virginia', 
+        state: ['WestVirginia'],
       },
       {
         name: 'Catherine Cortez Masto',
         rating: 'High ratings from environmental advocates',
         description: 'Strong advocate for renewable energy and climate action',
-        party: 'democratic',
+        party: Party.Democratic,
         link: 'https://www.cortezmasto.senate.gov/news/press-releases/cortez-masto-introduces-legislation-to-create-a-national-climate-service-corps/',
-        state: 'Nevada', 
+        state: ['Nevada'],
       },
       {
         name: 'Sherrod Brown',
         rating: 'High ratings from environmental advocates',
         description: 'Advocates for sustainable jobs and clean energy transition',
-        party: 'democratic',
+        party: Party.Democratic,
         link: 'https://www.brown.senate.gov/newsroom/press/release/brown-climate-change-threat-economy',
-        state: 'Ohio', 
+        state: ['Ohio'],
       },
       {
         name: 'Jon Tester',
         rating: 'Discusses importance of conservation',
         description: 'Supports renewable energy, emphasizes resource extraction',
-        party: 'democratic',
+        party: Party.Democratic,
         link: 'https://www.tester.senate.gov/about/issues/energy/',
-        state: 'Montana', 
+        state: ['Montana'],
       },
       {
         name: 'Tammy Baldwin',
         rating: 'High ratings from environmental groups',
         description: 'Strong advocate for climate action and renewable energy',
-        party: 'democratic',
+        party: Party.Democratic,
         link: 'https://www.baldwin.senate.gov/news/press-releases/us-senator-tammy-baldwin-helps-introduce-legislation-to-achieve-net-zero-greenhouse-gas-emissions',
-        state: 'Wisconsin', 
+        state: ['Wisconsin'],
       },
       {
         name: 'Matt Gaetz',
         rating: '25% from LCV.',
         description: 'Skeptical of climate change; focuses on economic growth.',
-        party: 'republican',
+        party: Party.Republican,
         link: 'https://www.newsweek.com/matt-gaetz-voted-against-fema-funding-before-hurricane-helene-hit-1961501',
-        state: 'Florida', 
+        state: ['Florida'],
       },
     ],
   },
@@ -120,8 +124,8 @@ const candidates: CandidateGroup[] = [
         rating: '90% from LCV.',
         description: 'Strong advocate for climate action; emphasizes urgency in addressing climate change.',
         link: 'https://www.npr.org/2024/06/25/nx-s1-5006573/trump-election-2024-climate-change-fossil-fuels',
-        party: 'democratic',
-        state: 'Florida', 
+        party: Party.Democratic,
+        state: ['Florida'],
       },
     ],
   },
@@ -131,9 +135,12 @@ const getCandidatesForState = (state: string, candidates: CandidateGroup[]) => {
   return candidates
     .map(group => ({
       ...group,
-      items: group.items.filter(candidate => candidate.state.toLowerCase() === state.toLowerCase()),
+      items: group.items.filter(candidate =>
+        candidate.state.map(s => s.toLowerCase()).includes(state.toLowerCase()) 
+        || candidate.state.includes('All') // Include candidates for 'All' states
+      ),
     }))
-    .filter(group => group.items.length > 0); 
+    .filter(group => group.items.length > 0);
 };
 
 export default function Who() {
@@ -159,14 +166,14 @@ export default function Who() {
               {/* Group Header */}
               <div
                 className={`flex items-center justify-center text-center who-help-${groupIndex + 1} p-4 md:p-8`}
-                style={{ gridRow: `span ${group.items.length}` }} // Dynamic row span
+                style={{ gridRow: `span ${group.items.length}` }} 
               >
                 {group.group}
               </div>
 
               {/* Candidate Items */}
               {group.items.map((candidate, itemIndex) => {
-                const partyColor = candidate.party === 'democratic' ? '#0033A0' : '#C8102E';
+                const partyColor = candidate.party === Party.Democratic ? '#0033A0' : '#C8102E';
 
                 return (
                   <div

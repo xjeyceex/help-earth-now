06410a67bff0d76c6b7b207efb6b6121ffc68717
Jc Miguel <jcmiguel.beltran@gmail.com>
2024-10-06 15:12:34 +0800

    fix br issue in recent what change, making div full

--- app/v3/what/what.tsx
+++ app/v3/what/what.tsx
@@ -73,7 +73,7 @@ export default function What() {
 
     return (
         <>
-            <div className="grid grid-cols-3 how-help">
+            <div className="grid grid-cols-3 how-help w-full">
                 <div className="text-center questions-1 p-12">
                     What can I do for free?
                 </div>

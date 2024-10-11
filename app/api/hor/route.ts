448c9ffcc008935b35a18da97594095a95d3c0ca
Jc Miguel <jcmiguel.beltran@gmail.com>
2024-10-11 09:47:52 +0800

    database testing - 4 production

--- app/api/hor/route.ts
+++ app/api/hor/route.ts
@@ -59,7 +59,7 @@ export async function GET(req: NextRequest) {
     const horsData = await getSheetsData();
     return NextResponse.json(horsData, {
       headers: {
-        'Cache-Control': 'no-store',
+        'Cache-Control': 'public, max-age=60, must-revalidate',
         'Access-Control-Allow-Origin': '*',  
         'Access-Control-Allow-Methods': 'GET, POST',
         'Access-Control-Allow-Headers': 'Content-Type',

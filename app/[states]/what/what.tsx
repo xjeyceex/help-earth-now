ec35d522689628507029a79f6bc242f94cf10147
Jc Miguel <jcmiguel.beltran@gmail.com>
2024-10-11 19:40:00 +0800

    make the right side in what section more narrower

--- app/[states]/what/what.tsx
+++ app/[states]/what/what.tsx
@@ -1,36 +1,32 @@
-'use client';
 import Link from "next/link";
-import { useParams } from "next/navigation";
-
 export default function What() {
-    const params = useParams()
-    const state = params.states
 
     return (
         <>
-            <div className="grid grid-cols-3 how-help w-full" id='what'>
-                <div className="flex text-center questions-1 p-12 items-center">
+            <div className="grid grid-cols-11 how-help w-full" id='what'>
+                <div className="flex items-center col-span-3 text-center questions-1 p-12">
                     What can I do for free?
                 </div>
-                <div className="col-span-2 answer-1 p-12">
+                <div className="col-span-8 answer-1 p-12">
                     Share this site with others
                     <br />
-                    <Link href={`${state}/who`} className="text-blue-500 underline hover:text-blue-700">
+                    <Link href='/who' className="text-blue-500 underline hover:text-blue-700">
                         Vote for people who care &rarr;
-                    </Link>
-                    <br />
+                    </Link>                    <br />
                     Burn less: fewer fires, drive less
                 </div>
-                <div className=" flex items-center text-center questions-2 p-12">
+
+                <div className="flex items-center col-span-3 text-center questions-2 p-12">
                     What if I can spend a little?
                 </div>
-                <div className="flex items-center col-span-2 answer-2 p-12">
+                <div className="flex items-center col-span-8 answer-2 p-12">
                     Insulate your doors & windows better
                 </div>
-                <div className="flex items-center text-center questions-3 p-12">
+
+                <div className="flex items-center col-span-3 text-center questions-3 p-12">
                     What if I can spend more?
                 </div>
-                <div className="col-span-2 answer-3 p-12">
+                <div className="col-span-8 answer-3 p-12">
                     Install solar cells <br />
                     Change appliances from gas to electricity <br />
                     Upgrade to energy efficient windows

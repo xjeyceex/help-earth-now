8ec8287415f5bc19cbaff9392a8f53cf87535caf
Jc Miguel <jcmiguel.beltran@gmail.com>
2024-10-15 06:49:53 +0800

    enhance floating icons

--- app/components/FixSocialIcon.tsx
+++ app/components/FixSocialIcon.tsx
@@ -1,47 +1,47 @@
 import React from "react";
-import { FaLinkedin, FaGithub, FaInstagram, FaFacebook, FaTwitter } from "react-icons/fa";
+import { FaLinkedin, FaInstagram, FaFacebook, FaTwitter } from "react-icons/fa";
 import Link from "next/link";
 
 function FixSocialIcon() {
   return (
     <div className="lg:fixed lg:top-1/2 lg:right-6 lg:transform lg:-translate-y-1/2 lg:flex lg:items-center relative bottom-0 right-0 w-full p-4 lg:w-auto lg:p-0">
-      <ul className="flex justify-center space-x-6 lg:block lg:space-x-0 lg:space-y-8">
+      <ul className="flex justify-center space-x-6 lg:block lg:space-x-0 lg:space-y-12">
         <li>
           <Link
-            href=""
-            // target="_blank"
+            href="https://www.linkedin.com" // Add your link
+            target="_blank"
             rel="noopener noreferrer"
-            className="text-4xl text-gray-500 hover:text-blue-500 dark:text-white"
+            className="text-4xl text-gray-500 hover:text-blue-500 transition-transform transform hover:scale-110"
           >
             <FaLinkedin />
           </Link>
         </li>
         <li>
           <Link
-            href=""
-            // target="_blank"
+            href="https://www.instagram.com" // Add your link
+            target="_blank"
             rel="noopener noreferrer"
-            className="text-4xl text-gray-500 hover:text-blue-500 dark:text-white"
+            className="text-4xl text-gray-500 hover:text-blue-500 transition-transform transform hover:scale-110"
           >
             <FaInstagram />
           </Link>
         </li>
         <li>
           <Link
-            href=""
-            // target="_blank"
+            href="https://www.facebook.com" // Add your link
+            target="_blank"
             rel="noopener noreferrer"
-            className="text-4xl text-gray-500 hover:text-blue-500 dark:text-white"
+            className="text-4xl text-gray-500 hover:text-blue-500 transition-transform transform hover:scale-110"
           >
             <FaFacebook />
           </Link>
         </li>
         <li>
           <Link
-            href=""
-            // target="_blank"
+            href="https://www.twitter.com" // Add your link
+            target="_blank"
             rel="noopener noreferrer"
-            className="text-4xl text-gray-500 hover:text-blue-500 dark:text-white"
+            className="text-4xl text-gray-500 hover:text-blue-500 transition-transform transform hover:scale-110"
           >
             <FaTwitter />
           </Link>

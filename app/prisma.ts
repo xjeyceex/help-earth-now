74a61e1876672c948c839ea0c12a06d39e0851f7
Jc Miguel <jcmiguel.beltran@gmail.com>
2024-10-15 01:12:19 +0800

    prisma experiment

--- app/prisma.ts
+++ app/prisma.ts
@@ -1,5 +1,12 @@
+import path from 'path';
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: `file:${path.resolve(__dirname, './app/prisma/dev.db')}`,
+    },
+  },
+});
 
 export { prisma };

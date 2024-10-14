063eaff1d45b319939d55b1d7f4c95174c65efad
Jc Miguel <jcmiguel.beltran@gmail.com>
2024-10-14 18:55:11 +0800

    button enhancement in v4

--- app/v4/header.tsx
+++ app/v4/header.tsx
@@ -104,7 +104,7 @@ export default function Header() {
                   ))}
                 </ul>
               </div>
-              <Link href="#what" className="mb-5 border border-gray-300 rounded-lg hover:bg-gray-100 inline-flex items-center justify-center px-6 py-4 mr-3 text-base font-medium text-center dark:text-white text-gray-900 dark:hover:bg-gray-700 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+              <Link href="#what" className="mb-5 border border-gray-400 rounded-full hover:bg-gray-100 inline-flex items-center justify-center px-6 py-4 mr-3 text-base font-medium text-center dark:text-white text-gray-900 dark:hover:bg-gray-700 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 What can I do?
                 <svg className="w-6 h-6 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path>

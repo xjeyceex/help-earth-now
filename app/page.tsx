38bad5ddcbe47170b3a10e7bde8572d548954631
Jc Miguel <jcmiguel.beltran@gmail.com>
2024-10-24 11:52:17 +0800

    remove v4

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import NavbarThree from "./v4/navbar-v4";
 import CookieConsent from "./components/CookieConsent";
 import Footer from "./components/Footer";
 import FeedbackButton from "./components/FeedBack";
+import WhyItMatters from "./components/WhyItMatters";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <NavbarThree/>
       <Header />
+      <WhyItMatters/>
       <CookieConsent/>
       <Footer/>
       <FeedbackButton/>

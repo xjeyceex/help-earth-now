3bf8f29d07ae39b40f07485a4af80beb84fdd908
Jc Miguel <jcmiguel.beltran@gmail.com>
2024-10-25 21:21:05 +0800

    cookie

--- app/components/CookieConsent.tsx
+++ app/components/CookieConsent.tsx
@@ -70,7 +70,7 @@ const CookieConsent = () => {
           onClick={handleConsent}
           className="bg-blue-600 text-white py-2 px-4 rounded-full hover:bg-blue-700 transition"
         >
-          Ok
+          OK
         </button>
       </div>
     </div>

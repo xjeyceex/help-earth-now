4fb557af2bb6ef76c8fcb6e3d04cfe025c52e884
Jc Miguel <jcmiguel.beltran@gmail.com>
2024-10-26 09:09:27 +0800

    remove slash

--- app/v4/navbar-v4.tsx
+++ app/v4/navbar-v4.tsx
@@ -257,7 +257,7 @@ export default function NavbarThree() {
                 onChange={handleCountyChange}
                 className="block w-full px-3 py-2 border border-gray-300 rounded-md text-gray-700 bg-gray-50 hover:bg-gray-100 transition"
               >
-                <option value="/">Select County</option>
+                <option value="">Select County</option>
                 {counties.map((county, index) => (
                   <option key={index} value={county} className="text-gray-700">
                     {county}

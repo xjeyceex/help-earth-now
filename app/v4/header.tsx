86e3443459ff3969b937b1789b6f67dedb75d053
xjeyceex <jcmiguel.beltran@gmail.com>
2025-02-10 23:52:22 +0800

    margin header py of climate change is hurting..

--- app/v4/header.tsx
+++ app/v4/header.tsx
@@ -91,7 +91,7 @@ export default function Header() {
       ) : (
         <>
           <section className="bg-gray-50 dark:bg-gray-900 lg:px-16">
-            <div className="grid max-w-screen-2xl px-6 py-10 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+            <div className="grid max-w-screen-2xl px-6 py-6 mx-auto lg:gap-8 xl:gap-0 lg:py-8 lg:pb-10 lg:grid-cols-12">
               <div className="mr-auto place-self-center lg:col-span-7 space-y-3">
                 <h1 className="max-w-2xl mb-4 font-extrabold tracking-tight leading-none md:text-6xl xl:text-7xl text-4xl dark:text-white">
                   Climate change is hurting us <span className="font-bold inline-block">all</span> - {' '}

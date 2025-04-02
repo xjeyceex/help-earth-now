e9f25cd0596b291e95ad0ed5d4cada8b3761bf49
xjeyceex <jcmiguel.beltran@gmail.com>
2025-04-02 22:53:00 +0800

    enhance whyitmatters section specifically modal

--- components/WhyItMatters.tsx
+++ components/WhyItMatters.tsx
@@ -3,7 +3,10 @@ import Link from 'next/link';
 import Image from 'next/image'; // Import Image from next/image
 import React, { useState, useRef, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import {
+  faChevronLeft,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 const WhyItMatters: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -26,59 +29,84 @@ const WhyItMatters: React.FC = () => {
     {
       image: '/wildfire.jpg',
       title: 'Wildfires',
-      description: 'Increasing temperatures have led to more frequent and intense wildfires, devastating forests and wildlife, and endangering human lives.',
-      moreInfo: 'Wildfires are exacerbated by climate change, leading to more severe damage to ecosystems and releasing significant amounts of CO2 into the atmosphere, worsening global warming. These fires threaten air quality, contribute to respiratory diseases, and force communities to evacuate, resulting in economic losses and long-term psychological impacts on affected populations.',
+      description:
+        'Increasing temperatures have led to more frequent and intense wildfires, devastating forests and wildlife, and endangering human lives.',
+      moreInfo:
+        'Wildfires are exacerbated by climate change, leading to more severe damage to ecosystems and releasing significant amounts of CO2 into the atmosphere, worsening global warming. These fires threaten air quality, contribute to respiratory diseases, and force communities to evacuate, resulting in economic losses and long-term psychological impacts on affected populations.',
       learnMore: 'https://www.who.int/health-topics/wildfires',
     },
     {
       image: '/sea-level.jpg',
       title: 'Rising Sea Levels',
-      description: 'Coastal cities around the world are threatened by rising sea levels, displacing communities and disrupting ecosystems.',
-      moreInfo: 'Rising sea levels result from melting ice caps and glaciers and thermal expansion of seawater, leading to the loss of homes, arable land, and critical infrastructure. Increased flooding and erosion threaten freshwater supplies and create significant economic challenges, particularly for coastal communities dependent on tourism and fishing.',
-      learnMore: 'https://www.climate.gov/news-features/understanding-climate/climate-change-global-sea-level',
+      description:
+        'Coastal cities around the world are threatened by rising sea levels, displacing communities and disrupting ecosystems.',
+      moreInfo:
+        'Rising sea levels result from melting ice caps and glaciers and thermal expansion of seawater, leading to the loss of homes, arable land, and critical infrastructure. Increased flooding and erosion threaten freshwater supplies and create significant economic challenges, particularly for coastal communities dependent on tourism and fishing.',
+      learnMore:
+        'https://www.climate.gov/news-features/understanding-climate/climate-change-global-sea-level',
     },
     {
       image: '/drought.jpg',
       title: 'Droughts',
-      description: 'Prolonged droughts are becoming more common, threatening food and water supplies for millions of people, causing economic and social instability.',
-      moreInfo: 'Droughts have far-reaching consequences on agriculture, drinking water supplies, and food security, leading to famine and displacement in vulnerable regions. The agricultural sector suffers significantly, with crop failures and livestock losses, which can escalate food prices and increase competition for resources, leading to social unrest.',
+      description:
+        'Prolonged droughts are becoming more common, threatening food and water supplies for millions of people, causing economic and social instability.',
+      moreInfo:
+        'Droughts have far-reaching consequences on agriculture, drinking water supplies, and food security, leading to famine and displacement in vulnerable regions. The agricultural sector suffers significantly, with crop failures and livestock losses, which can escalate food prices and increase competition for resources, leading to social unrest.',
       learnMore: 'https://www.drought.gov/current-conditions',
     },
     {
       image: '/storms.jpg',
       title: 'Severe Storms',
-      description: 'Climate change is fueling more intense hurricanes, typhoons, and cyclones, causing widespread damage to communities and infrastructure.',
-      moreInfo: 'Warmer ocean temperatures and changing weather patterns are contributing to more powerful and destructive storms, displacing millions and costing billions in damages. The increase in storm frequency and intensity leads to higher insurance premiums, infrastructure repair costs, and long-term displacement for communities, significantly affecting local economies and recovery efforts.',
-      learnMore: 'https://www.climate.gov/news-features/understanding-climate/climate-change-global-sea-level',
+      description:
+        'Climate change is fueling more intense hurricanes, typhoons, and cyclones, causing widespread damage to communities and infrastructure.',
+      moreInfo:
+        'Warmer ocean temperatures and changing weather patterns are contributing to more powerful and destructive storms, displacing millions and costing billions in damages. The increase in storm frequency and intensity leads to higher insurance premiums, infrastructure repair costs, and long-term displacement for communities, significantly affecting local economies and recovery efforts.',
+      learnMore:
+        'https://www.climate.gov/news-features/understanding-climate/climate-change-global-sea-level',
     },
     {
       image: '/heatwave.jpg',
       title: 'Heatwaves',
-      description: 'Intense heatwaves are becoming more intense and prolonged, endangering public health, agriculture, and energy supplies.',
-      moreInfo: 'Extreme heat events cause heat-related illnesses and deaths, strain power grids, and reduce crop yields, exacerbating food insecurity. Vulnerable populations, including the elderly and those with pre-existing health conditions, are at greater risk. The agricultural sector also faces challenges as heat stress on crops reduces yields and quality, impacting food supply and prices.',
-      learnMore: 'https://www.who.int/news-room/fact-sheets/detail/climate-change-heat-and-health',
+      description:
+        'Intense heatwaves are becoming more intense and prolonged, endangering public health, agriculture, and energy supplies.',
+      moreInfo:
+        'Extreme heat events cause heat-related illnesses and deaths, strain power grids, and reduce crop yields, exacerbating food insecurity. Vulnerable populations, including the elderly and those with pre-existing health conditions, are at greater risk. The agricultural sector also faces challenges as heat stress on crops reduces yields and quality, impacting food supply and prices.',
+      learnMore:
+        'https://www.who.int/news-room/fact-sheets/detail/climate-change-heat-and-health',
     },
     {
       image: '/melting.jpg',
       title: 'Melting Ice Caps',
-      description: 'The polar ice caps are melting at an alarming rate, contributing to sea level rise and threatening polar ecosystems.',
-      moreInfo: "The loss of Arctic and Antarctic ice disrupts global weather patterns, accelerates sea level rise, and puts species like polar bears and penguins at risk of extinction. The melting ice also impacts Indigenous communities that rely on these ecosystems for their livelihoods, as well as global climate patterns by reducing the Earth's albedo effect, which helps regulate temperatures.",
-      learnMore: 'https://www.nationalgeographic.com/environment/article/antarctic-ice-melt',
+      description:
+        'The polar ice caps are melting at an alarming rate, contributing to sea level rise and threatening polar ecosystems.',
+      moreInfo:
+        "The loss of Arctic and Antarctic ice disrupts global weather patterns, accelerates sea level rise, and puts species like polar bears and penguins at risk of extinction. The melting ice also impacts Indigenous communities that rely on these ecosystems for their livelihoods, as well as global climate patterns by reducing the Earth's albedo effect, which helps regulate temperatures.",
+      learnMore:
+        'https://www.nationalgeographic.com/environment/article/antarctic-ice-melt',
     },
     {
       image: '/biodiversity.jpg',
       title: 'Biodiversity Loss',
-      description: 'Many species are being driven to extinction due to habitat loss, extreme weather, and changing ecosystems.',
-      moreInfo: 'Climate change disrupts ecosystems, leading to loss of biodiversity which in turn affects food chains, human livelihoods, and the natural environment. As species struggle to adapt or migrate to suitable habitats, ecosystems become imbalanced, affecting ecosystem services such as pollination, clean water, and climate regulation, which are vital for human survival.',
-      learnMore: 'https://www.iucn.org/resources/issues-briefs/biodiversity-and-climate-change',
+      description:
+        'Many species are being driven to extinction due to habitat loss, extreme weather, and changing ecosystems.',
+      moreInfo:
+        'Climate change disrupts ecosystems, leading to loss of biodiversity which in turn affects food chains, human livelihoods, and the natural environment. As species struggle to adapt or migrate to suitable habitats, ecosystems become imbalanced, affecting ecosystem services such as pollination, clean water, and climate regulation, which are vital for human survival.',
+      learnMore:
+        'https://www.iucn.org/resources/issues-briefs/biodiversity-and-climate-change',
     },
-  ];  
+  ];
 
   const duplicatedItems = [...items, ...items, ...items]; // Duplicate items to make scroll smooth in both directions
   const cardContainerRef = useRef<HTMLDivElement>(null);
   const modalRef = useRef<HTMLDivElement>(null); // Reference for modal content
 
-  const handleCardClick = (item: { title: string; description: string; image: string; moreInfo: string; learnMore: string }) => {
+  const handleCardClick = (item: {
+    title: string;
+    description: string;
+    image: string;
+    moreInfo: string;
+    learnMore: string;
+  }) => {
     setModalContent(item);
     setIsOpen(true);
   };
@@ -133,7 +161,10 @@ const WhyItMatters: React.FC = () => {
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
         closeModal();
       }
     };
@@ -149,16 +180,20 @@ const WhyItMatters: React.FC = () => {
     };
   }, [isOpen]);
 
-
   return (
     <section className="bg-gray-100 dark:bg-gray-900 py-8 px-4 w-full relative lg:px-20">
       <div className="max-w-screen-2xl mx-auto text-center">
         <p className="text-2xl text-gray-700 dark:text-gray-300">
-          Climate change is affecting every corner of the planet, from extreme weather events to rising sea levels. By taking action now, we can slow down these effects and protect our future.
+          Climate change is affecting every corner of the planet, from extreme
+          weather events to rising sea levels. By taking action now, we can slow
+          down these effects and protect our future.
         </p>
 
         <div className="mt-6 relative overflow-hidden">
-          <div ref={cardContainerRef} className="flex space-x-8 overflow-x-auto snap-x snap-mandatory scrollbar-hide p-4">
+          <div
+            ref={cardContainerRef}
+            className="flex space-x-8 overflow-x-auto snap-x snap-mandatory scrollbar-hide p-4"
+          >
             {duplicatedItems.map((item, index) => (
               <div
                 key={index}
@@ -175,7 +210,9 @@ const WhyItMatters: React.FC = () => {
                 <div className="relative p-6 text-gray-900 dark:text-gray-100 text-center">
                   <h3 className="text-3xl font-semibold mb-3">{item.title}</h3>
                   <hr className="border-t-2 border-gray-300 dark:border-gray-700 my-6 mx-auto w-3/4" />
-                  <p className="mt-2 text-gray-700 dark:text-gray-300">{item.description}</p>
+                  <p className="mt-2 text-gray-700 dark:text-gray-300">
+                    {item.description}
+                  </p>
                 </div>
               </div>
             ))}
@@ -183,16 +220,16 @@ const WhyItMatters: React.FC = () => {
 
           {/* Navigation buttons */}
           <div className="absolute top-1/2 transform -translate-y-1/2 left-5">
-            <button 
-              className="text-white bg-blue-500 dark:text-white hover:bg-blue-600 w-12 h-12 rounded-full flex items-center justify-center" 
+            <button
+              className="text-white bg-blue-500 dark:text-white hover:bg-blue-600 w-12 h-12 rounded-full flex items-center justify-center"
               onClick={scrollLeft}
             >
               <FontAwesomeIcon icon={faChevronLeft} className="text-1xl" />
             </button>
           </div>
           <div className="absolute top-1/2 transform -translate-y-1/2 right-5">
-            <button 
-              className="text-white bg-blue-500 dark:text-white hover:bg-blue-600 w-12 h-12 rounded-full flex items-center justify-center" 
+            <button
+              className="text-white bg-blue-500 dark:text-white hover:bg-blue-600 w-12 h-12 rounded-full flex items-center justify-center"
               onClick={scrollRight}
             >
               <FontAwesomeIcon icon={faChevronRight} className="text-1xl" />
@@ -204,28 +241,61 @@ const WhyItMatters: React.FC = () => {
       {/* Modal content */}
       {isOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center px-6">
-          <div ref={modalRef} className="bg-white dark:bg-gray-800 max-w-lg mx-auto p-8 rounded-lg shadow-lg relative text-center">
-            <button
-              onClick={closeModal}
-              className="absolute top-4 right-4 text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-100 text-2xl"
-            >
-              &times;
-            </button>
-            <Image src={modalContent.image} alt={modalContent.title} width={600} height={400} className="w-full h-64 object-cover rounded-lg mb-4" />
-            <h3 className="text-2xl font-bold text-gray-900 dark:text-gray-100 mt-4">{modalContent.title}</h3>
-            <p className="mt-2 text-gray-700 dark:text-gray-300">{modalContent.moreInfo}</p>
-            <Link
-              href={modalContent.learnMore}
-              target="_blank"
-              className="inline-block mt-4 px-6 py-3 text-white bg-blue-500 dark:bg-blue-600 hover:bg-blue-600 dark:hover:bg-blue-700 rounded-lg"
-            >
-              Learn More
-            </Link>
+          <div
+            ref={modalRef}
+            className="bg-white dark:bg-gray-800 max-w-lg w-full mx-auto p-6 rounded-lg shadow-lg relative"
+          >
+            {/* Header */}
+            <div className="flex justify-between items-center">
+              <h3 className="text-xl font-bold text-gray-900 dark:text-gray-100">
+                Drought
+              </h3>
+              <button
+                onClick={closeModal}
+                className="text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-100 text-2xl rounded-full w-8 h-8 flex items-center justify-center bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 transition-colors duration-200"
+              >
+                &times;
+              </button>
+            </div>
+            <hr className="my-4 border-gray-300 dark:border-gray-600 w-full" />
+
+            {/* Image */}
+            <Image
+              src={modalContent.image}
+              alt={modalContent.title}
+              width={600}
+              height={400}
+              className="w-full h-64 object-cover rounded-lg mb-4"
+            />
+
+            {/* Description */}
+            <p className="text-gray-700 dark:text-gray-300">
+              {modalContent.moreInfo}
+            </p>
+
+            {/* Divider */}
+            <hr className="my-4 border-gray-300 dark:border-gray-600 w-full" />
+
+            {/* Buttons */}
+            <div className="flex justify-center gap-4">
+              <button
+                onClick={closeModal}
+                className="px-6 py-3 text-gray-700 dark:text-gray-300 bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 rounded-lg"
+              >
+                Close
+              </button>
+              <Link
+                href={modalContent.learnMore}
+                target="_blank"
+                className="px-6 py-3 text-white bg-blue-500 dark:bg-blue-600 hover:bg-blue-600 dark:hover:bg-blue-700 rounded-lg"
+              >
+                Learn More
+              </Link>
+            </div>
           </div>
         </div>
       )}
     </section>
-
   );
 };
 

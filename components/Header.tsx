ba0b93ff77f730ab15b34538f1adb1862e87f09a
Jc Miguel Beltran <jcmiguel.beltran@gmail.com>
2025-08-13 22:08:07 +0800

    skeleton loading header

--- components/Header.tsx
+++ components/Header.tsx
@@ -215,7 +215,7 @@ export default function Header() {
                 <ul className="space-y-3 pl-8">
                   {[1, 2, 3].map((item) => (
                     <li key={item} className="flex items-center w-1/3">
-                      <div className="h-2 w-2 bg-gray-300 dark:bg-gray-600 rounded-full mr-2"></div>
+                      <div className="h-3 w-3 bg-gray-300 dark:bg-gray-600 rounded-full mr-2"></div>
                       <div className="h-6 bg-gray-200 dark:bg-gray-700 rounded animate-pulse w-5/6"></div>
                     </li>
                   ))}
